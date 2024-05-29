89116f6597226f86124919e13b96064d1977a25c
Vivicaty <101065613+VivicatcHub@users.noreply.github.com>
2024-05-29 18:11:05 +0200

    Update Chapitre.js
    Update where_solov => WhereSoloLeveling

--- SoloLeveling/Chapitre.js
+++ SoloLeveling/Chapitre.js
@@ -1,20 +1,21 @@
+let ANIME = 'SoloLeveling';
 // Sélectionner le bouton
 const boutonModifierWhere = document.getElementById("modifierWhere");
-var where = localStorage.getItem('where_solov');
+var where = localStorage.getItem(`Where${ANIME}`);
 
 // Ajouter un écouteur d'événements pour le clic sur le bouton
 boutonModifierWhere.addEventListener("click", () => {
     // Afficher une boîte de dialogue (alert) avec un champ de saisie
     const nouvelleValeur = prompt(
         `À quel chapitre êtes-vous actuellement ? (Pour l'intant : ${localStorage.getItem(
-            "where_solov"
+            `Where${ANIME}`
         )})`
     );
     // Vérifier si l'utilisateur a cliqué sur OK
     if (nouvelleValeur !== null) {
         // Stocker la nouvelle valeur dans le localStorage avec la clé 'nom'
-        localStorage.setItem("where_solov", nouvelleValeur);
-        localStorage.setItem("modif", true);
+        localStorage.setItem(`Where${ANIME}`, nouvelleValeur);
+        localStorage.setItem("Modif", true);
         // Afficher un message pour confirmer que la valeur a été modifiée
         alert(
             "La valeur a été modifiée avec succès ! Nouvelle valeur : " +
